d430ccd7a08413e67b8401775923b93ae0a6836c
Pranshu Goyal <pranshu.ghps@gmail.com>
2022-01-08 15:51:01 +0530

    Update twenty_pct_strategy.py

--- twenty_pct_strategy.py
+++ twenty_pct_strategy.py
@@ -33,7 +33,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 # import os
 from pandasql import *
-from time import sleep
 from IPython.display import clear_output
 
 
@@ -76,28 +75,28 @@ master_data.head()
 # In[68]:
 
 
-#####LAST ONE MONTH DIFFERENCE####
-quandl.ApiConfig.api_key = 'bpiTuJQDVqfgdZeX_tVu'
-old_day=date.today()-timedelta(30)
-if(old_day.weekday()==5):
-    old_day=date.today()-timedelta(31)
-elif(old_day.weekday()==6):
-    old_day=date.today()-timedelta(29)
-else:
-    old_day=old_day
-historical_price=pd.DataFrame(columns=['code','name','close_price'])
+# #####LAST ONE MONTH DIFFERENCE####
+# quandl.ApiConfig.api_key = 'bpiTuJQDVqfgdZeX_tVu'
+# old_day=date.today()-timedelta(30)
+# if(old_day.weekday()==5):
+#     old_day=date.today()-timedelta(31)
+# elif(old_day.weekday()==6):
+#     old_day=date.today()-timedelta(29)
+# else:
+#     old_day=old_day
+# historical_price=pd.DataFrame(columns=['code','name','close_price'])
 
-for i in range(len(master_data)):
-    code='BSE/BOM'+str(master_data['Security Code'][i])
-    print(code)
-    print(i)
-    df=quandl.get(code, start_date=old_day, end_date=old_day)
-    cd=master_data['Security Code'][i]
-    name=master_data['Security Name'][i]
-    close_price=df['Close'][0]
-    print(close_price)
-    data={'code':cd,'name':name,'close_price':close_price}
-    historical_price=historical_price.append(data,ignore_index=True)
+# for i in range(len(master_data)):
+#     code='BSE/BOM'+str(master_data['Security Code'][i])
+#     print(code)
+#     print(i)
+#     df=quandl.get(code, start_date=old_day, end_date=old_day)
+#     cd=master_data['Security Code'][i]
+#     name=master_data['Security Name'][i]
+#     close_price=df['Close'][0]
+#     print(close_price)
+#     data={'code':cd,'name':name,'close_price':close_price}
+#     historical_price=historical_price.append(data,ignore_index=True)
     
 
 
@@ -149,29 +148,24 @@ b = BSE(update_codes = True)
 
 # In[72]:
 
-
-k=0
-while (k<10):
-    log=pd.DataFrame(columns=['timestamp','code''name','old_price','current_price','drop_pct','grow_pct'])
+log=pd.DataFrame(columns=['timestamp','code''name','old_price','current_price','drop_pct','grow_pct'])
+clear_output(wait=True)
+print('Running Again')
+for i in range(10):
+    code=master_data['Security Code'][i]
+    q=b.getQuote(str(code))
+    current=float(q['currentValue'])
+    old=float(historical_price[historical_price['code']==int(code)]['close_price'])
+    drop_pct=((old-current)/old)*100
+    grow_pct=((current-old)/old)*100
+    if(grow_pct>=5):
+        data={'timestamp':q['updatedOn'],'code': q['scripCode'],'name':q['securityID'],'old_price':old,'current_price':current,'drop_pct':drop_pct,'grow_pct':grow_pct}
+        log=log.append(data,ignore_index=True)
+if(len(log)==0):   
     clear_output(wait=True)
-    print('Running Again')
-    for i in range(10):
-        code=master_data['Security Code'][i]
-        q=b.getQuote(str(code))
-        current=float(q['currentValue'])
-        old=float(historical_price[historical_price['code']==int(code)]['close_price'])
-        drop_pct=((old-current)/old)*100
-        grow_pct=((current-old)/old)*100
-        if(grow_pct>=5):
-            data={'timestamp':q['updatedOn'],'code': q['scripCode'],'name':q['securityID'],'old_price':old,'current_price':current,'drop_pct':drop_pct,'grow_pct':grow_pct}
-            log=log.append(data,ignore_index=True)
-    if(len(log)==0):   
-        clear_output(wait=True)
-        print('No Records')
-    else:
-        clear_output(wait=True)
-        print(log)
-    sleep(2)
-    k=k+1
+    print('No Records')
+else:
+    clear_output(wait=True)
+    print(log)
        
 
